908f2b852f70c5dfc93a958a13bb271948e4e572
Fabian Müller <fabianmueller5@gmx.de>
2014-12-12 06:30:29 +0100

    Fix includes.

--- src/gui_qt/utils.cpp
+++ src/gui_qt/utils.cpp
@@ -1,27 +1,34 @@
-#include <gui_qt/utils.hpp>
-#include <qt5/QtQuick/QQuickView>
-#include <qt5/QtGui/QGuiApplication>
-#include <qt5/QtQml/QQmlApplicationEngine>
-#include <qt5/QtCore/QString>
+#include "gui_qt/utils.hpp"
+
+#include "widget.hpp"
+
+#include <QtQuick/QQuickView>
+#include <QtGui/QGuiApplication>
+#include <QtQml/QQmlApplicationEngine>
+#include <QtCore/QString>
 
 namespace gui_qt {
 
-int show_application(const std::string& qml_file_path, int argc, char** argv) {
+int show_application(const std::string& qml_file_path,
+                     int argc, char** argv) {
 
-	QGuiApplication app(argc, argv);
-	QQmlApplicationEngine engine(QString(qml_file_path.c_str()));
+    QGuiApplication app(argc, argv);
+    QQmlApplicationEngine engine(QString(qml_file_path.c_str()));
 
-	return app.exec();
+//    qmlRegisterType<gui_qt::widget>("com.myself", 1, 0, "MyWidget");
+
+    return app.exec();
 }
 
 void show_quick(const std::string& qml_file_path) {
 
-	int argc = 0;
-	QGuiApplication app(argc, NULL);
-	QQuickView view;
-	view.setSource(QUrl::fromLocalFile(QString(qml_file_path.c_str())));
-	view.show();
-	app.exec();
+    int argc = 0;
+    QGuiApplication app(argc, NULL);
+    QQuickView view;
+    view.setSource(QUrl::fromLocalFile(
+                       QString(qml_file_path.c_str())));
+    view.show();
+    app.exec();
 }
 
 
