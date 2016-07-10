80bcd4deb9b246c9bd1c485257a2f01a89321d91
Celtic Minstrel <celtic.minstrel.ca@some.place>
2016-07-10 10:07:32 -0400

    Fix compile errors in unit tests

--- src/tests/utils/game_config_manager.cpp
+++ src/tests/utils/game_config_manager.cpp
@@ -64,13 +64,10 @@ namespace test_utils {
 			setlocale(LC_ALL, "English");
 #else
 			std::setlocale(LC_ALL, "C");
-			translation::init();
 #endif
 			const std::string& intl_dir = filesystem::get_intl_dir();
 			translation::bind_textdomain("wesnoth", intl_dir.c_str(), "UTF-8");
 			translation::bind_textdomain("wesnoth-lib", intl_dir.c_str(), "UTF-8");
-			translation::set_default_textdomain("wesnoth");
-
 
 			font::load_font_config();
 			gui2::init();
